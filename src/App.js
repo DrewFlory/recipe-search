8ad1ccc281652347fd4dc4d940ed0dcc770fd8c2
John Doe <ajflory9@yahoo.com>
2018-08-25 02:04:07 -0400

    Almost completed/ bug

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form from './components/Form';
-import Recipes from './components/Recipes';
-
-
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
 
 const API_KEY = "2622bd912820d51980959a935609f4f4";
-
 
 class App extends Component {
   state = {
     recipes: []
   }
   getRecipe = async (e) => {
-    //Grabs Input Value 
     const recipeName = e.target.elements.recipeName.value;
-    //Prevents Full Page Refresh
     e.preventDefault();
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=12`);
-    //Grabbing data and converting to JSON format
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+    
     const data = await api_call.json();
-    this.setState({ recipes: data.recipes })
-    console.log(this.state.recipes)
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  }
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes });
+  }
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
   }
   render() {
     return (
@@ -29,10 +33,10 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-       <Form getRecipe={this.getRecipe}/>
-       <Recipes recipes={this.state.recipes}/>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
       </div>
-      );
+    );
   }
 }
 
